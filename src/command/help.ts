df6014e51c55558e75cc88985ee0d0ca40a2f2ff
ffdd270 <ffdd270@gmail.com>
2020-09-19 16:06:26 +0900

    Help 분리.

--- src/command/help.ts
+++ src/command/help.ts
@@ -14,7 +14,7 @@ class HelpCommand
 export class HelpCommands
 {
     static NormalCommands : HelpCommand[] = help;
-    static OperatorCommands : HelpCommand[] = help_all;
+    static OperatorCommands : HelpCommand[][] = help_all;
 
 
     constructor()
@@ -50,11 +50,40 @@ export class HelpCommands
         return result_string;
     }
 
-    static async ShowCommand()
+
+    static HelpRegex = /(\d)/;
+
+    static async ShowCommand( params : string | null)
     {
         let result_string = "";
+        let page = 1;
+
+        if (params != null)
+        {
+            let regex_result = HelpCommands.HelpRegex.exec( params );
+            if ( regex_result == null )
+            {
+                page = 1;
+            }
+            else
+            {
+                page = Number.parseInt( regex_result[1] );
+            }
+        }
+        else
+        {
+            page = 1;
+        }
+
+        if ( page < 0 || page > HelpCommands.OperatorCommands.length )
+        {
+            result_string = `이런. 페이지를 잘못 넣은 것 같은데. 0이상, ${HelpCommands.OperatorCommands.length}이하의 값을 넣어줘.`;
+            return result_string;
+        }
+
         result_string += "반가워. 이건 FLNPD Operator System의 설명서야. 혹시 궁금한 게 있으면, @KuroNeko를 태그해줘!\n\n" ;
-        result_string += HelpCommands.EachData( HelpCommands.OperatorCommands );
+        result_string += `이 페이지는 ${page}/${HelpCommands.OperatorCommands.length}야. 2 페이지를 보고 싶으면, !help-all 2 처럼 넣어줘.\n`;
+        result_string += HelpCommands.EachData( HelpCommands.OperatorCommands[page - 1] );
 
         return result_string;
     }
